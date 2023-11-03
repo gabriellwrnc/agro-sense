3bd3cbd6799793a1d0be52b923b5832eff4fe6f6
Gabriel Dicky Lawrence Jamrewav <90816980+gabriellwrnc@users.noreply.github.com>
2023-11-03 16:02:24 +0800

    Setup font and Install react-native-svg

---
+++ agrosense-frontend/agrosense/src/configs/Fonts.tsx
@@ -0,0 +1,24 @@
+export const FontFamily = {
+    reemkufiBold: 'ReemKufi-Bold',
+    reemkufiSemiBold: 'ReemKufi-SemiBold',
+    reemkufiMedium: 'ReemKufi-Medium',
+    reemkufiRegular: 'ReemKufi-Regular',
+    poppinsExtraBold: 'Poppins-ExtraBold',
+    poppinsBold: 'Poppins-Bold',
+    poppinsBoldItalic: 'Poppins-BoldItalic',
+    poppinsSemiBold: 'Poppins-SemiBold',
+    poppinsMedium: 'Poppins-Medium',
+    poppinsRegular: 'Poppins-Regular',
+};
+
+export const FontSize = {
+    xs: 14,
+    sm: 16,
+    md: 18,
+    lg: 20,
+    xl: 22,
+    xl1: 24,
+    xl2: 26,
+    xxl: 30,
+    xxxl: 36,
+};
--- agrosense-frontend/agrosense/src/screens/auth/signin/SignIn.tsx
+++ agrosense-frontend/agrosense/src/screens/auth/signin/SignIn.tsx
@@ -1,11 +1,39 @@
 import React from 'react';
 import { Text } from 'react-native-ui-lib';
 import { ScreenLayout } from '../../../components';
+import { FontFamily, FontSize } from '../../../configs';
 
 const SignIn: React.FC = () => {
     return (
         <ScreenLayout backgroundColor="light" flex center>
-            <Text>Signin Screen</Text>
+            <Text
+                style={{
+                    fontFamily: FontFamily.reemkufiBold,
+                    fontSize: FontSize.xl,
+                }}>
+                Reemkufi Bold
+            </Text>
+            <Text
+                style={{
+                    fontFamily: FontFamily.reemkufiRegular,
+                    fontSize: FontSize.xl,
+                }}>
+                Reemkufi Regular
+            </Text>
+            <Text
+                style={{
+                    fontFamily: FontFamily.poppinsBold,
+                    fontSize: FontSize.xl,
+                }}>
+                Poppins Bold
+            </Text>
+            <Text
+                style={{
+                    fontFamily: FontFamily.poppinsRegular,
+                    fontSize: FontSize.xl,
+                }}>
+                Poppins Regular
+            </Text>
         </ScreenLayout>
     );
 };

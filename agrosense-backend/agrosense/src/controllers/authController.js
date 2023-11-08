339946c6029507de7d80544392ab21ed98046c6f
Gabriel Dicky Lawrence Jamrewav <90816980+gabriellwrnc@users.noreply.github.com>
2023-11-08 19:50:32 +0800

    AS-10 Update sign up API

--- agrosense-backend/agrosense/src/controllers/authController.js
+++ agrosense-backend/agrosense/src/controllers/authController.js
@@ -2,11 +2,10 @@ import {
     createUserWithEmailAndPassword,
 } from 'firebase/auth';
 import { firebaseAuth } from '../utils';
+import { User } from '../models';
 
 export const signUp = async (req, res) => {
     const { name, email, phoneNumber, password } = req.body;
-
-    console.log('req.body', req.body)
 
     try {
         const firebaseResponse = await createUserWithEmailAndPassword(
@@ -15,9 +14,38 @@ export const signUp = async (req, res) => {
             password,
         );
 
+        const existingUser = await User.findOne({ email });
+
+        if (existingUser) {
+            return res
+                .status(409)
+                .json({ status: 'fail', message: 'Email already exist' });
+        }
+
+        const newUser = new User({
+            name,
+            email,
+            phoneNumber,
+            role: 'user',
+            firebaseId: firebaseResponse.user.uid,
+        });
+
+        await newUser.save();
+
         res.status(200).json({ status: 'success', message: firebaseResponse.user });
     } catch (error) {
-        console.log('error', error)
+        if (error.code === 'auth/email-already-in-use') {
+            return res
+                .status(409)
+                .json({ status: 'fail', message: 'Email already exist' });
+        }
+
+        if (error.code === 'auth/invalid-email') {
+            return res
+                .status(400)
+                .json({ status: 'fail', message: 'Invalid email' });
+        }
+
         res.status(500).json({ status: 'fail', message: error });
     }
 }
---
+++ agrosense-backend/agrosense/src/models/User.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    phoneNumber: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+    firebaseId: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    role: {
+        type: String,
+        required: true,
+        enum: ['admin', 'user'],
+        default: 'user',
+    },
+})
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
